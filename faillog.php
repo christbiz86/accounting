50659af6f61383262e5ec7d273e4d3148102eda1
Christian <christian@Christians-MacBook-Air.local>
2019-03-08 16:00:28 +0700

    accounting system

---
+++ faillog.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Login attempts info.
+*/
+$login_faillog = array (
+  0 => 
+  array (
+    '192.168.65.75' => 0,
+    'last' => '',
+  ),
+  1 => 
+  array (
+    '192.168.65.21' => 0,
+    'last' => '',
+  ),
+  2 => 
+  array (
+    '192.168.65.71' => 0,
+    'last' => '',
+  ),
+);
